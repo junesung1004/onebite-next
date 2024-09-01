bbba67032a051d9dc382c3f3a33783bd61dd76ec
junesung1004 <junecoding1004@gmail.com>
2024-09-01 21:30:15 +0900

    뼈대마크업

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ const imgs = [
   { img: `1.jpg`, name: "Korean Fried", price: "2000", kallory: "2" },
   { img: `2.jpg`, name: "Grilled", price: "2300", kallory: "5" },
   { img: `3.jpg`, name: "Fried", price: "200", kallory: "11" },
-  { img: `4.jpg`, name: "Deep Fried", price: "2300", kallory: "6" },
+  { img: `4.jpg`, name: "Deep Fried", price: "2300", kallory: "60" },
 ];
 
 export default function Card() {
