1dc36f7c6df2349ee712fe070442dbf15be0ee43
junesung1004 <junesung7979@gmail.com>
2024-09-04 00:18:59 +0900

    마크업단계

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 "use client"
 
-import Link from "next/link";
 import styles from "./page.module.scss";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import {motion} from "framer-motion";
 
 export default function App() {
   const router = useRouter();
@@ -13,13 +13,19 @@ export default function App() {
   useEffect(()=> {
     setTimeout(()=> {
       router.push('/home');
-        }, 2000)
-  },)
+        }, 3000)
+  },[router])
   
 
   return (
-    <div className={styles.container}>
-      <Image src="/images/logo.jpg" alt="logo" width={100} height={100}/>
-    </div>
+    <motion.div
+    className={styles.container}
+    initial={{ opacity: 0 }} // 초기 상태
+    animate={{ opacity: 1 }} // 애니메이션 상태
+    exit={{ opacity: 0 }} // 페이지 나갈 때 애니메이션
+    transition={{ duration: 1.5 }} // 애니메이션 지속 시간
+  >
+    <Image src="/images/logo.jpg" alt="logo" width={100} height={100} />
+  </motion.div>
   );
 }
