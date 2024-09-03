8e47e856addd5cb902c45fda3947edc22de33bad
junesung1004 <junesung7979@gmail.com>
2024-09-03 22:19:50 +0900

    마크업중

---
+++ src/app/page.js
@@ -0,0 +1,25 @@
+"use client"
+
+import Link from "next/link";
+import styles from "./page.module.scss";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function App() {
+  const router = useRouter();
+
+
+  useEffect(()=> {
+    setTimeout(()=> {
+      router.push('/home');
+        }, 3000)
+  },)
+  
+
+  return (
+    <div className={styles.container}>
+      <Image src="/images/logo.jpg" alt="logo" width={100} height={100}/>
+    </div>
+  );
+}
