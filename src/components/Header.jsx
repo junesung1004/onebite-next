8e253179a1c593281a04a09c20916a644faa14a2
junesung1004 <junesung7979@gmail.com>
2024-09-04 00:37:39 +0900

    후

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Link from "next/link";
 import React from "react";
 import styles from "./Header.module.scss";
@@ -10,16 +9,3 @@ export default function Header() {
     </div>
   );
 }
-=======
-import Link from "next/link";
-import React from "react";
-import styles from "./Header.module.scss";
-
-export default function Header() {
-  return (
-    <div className={styles.container}>
-      <Link href={"/"}>todayEat💓</Link>
-    </div>
-  );
-}
->>>>>>> bbba67032a051d9dc382c3f3a33783bd61dd76ec
--- src/components/global-layout.jsx
+++ src/components/global-layout.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
@@ -13,19 +12,3 @@ export default function GlobalLayout({ children }) {
     </div>
   );
 }
-=======
-import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import styles from "./global-layout.module.scss";
-
-export default function GlobalLayout({ children }) {
-  return (
-    <div className={styles.container}>
-      <Header />
-      <div className={styles.main}>{children}</div>
-      <Footer />
-    </div>
-  );
-}
->>>>>>> bbba67032a051d9dc382c3f3a33783bd61dd76ec
